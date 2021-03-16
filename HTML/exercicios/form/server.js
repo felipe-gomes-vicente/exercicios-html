68e745146098fa732aa614ac307c38d562c64f53
Felipe Gomes Vicente <felipegomes.vicente2@gmail.com>
2021-03-15 23:24:32 -0300

    atualização e novos exercicios

--- HTML/exercicios/form/server.js
+++ HTML/exercicios/form/server.js
@@ -2,9 +2,17 @@ const express = require('express')
 const app = express()
 app.use(express.json())
 
+app.use(express.urlencoded());
+
 app.post('/usuarios', (req, resp) => {
   console.log(req.body)
-  resp.send('<h1>Parabéns</h1>')
+  resp.send('<h1>Parabéns. Usuário Incluido!!!</h1>')
+})
+
+app.post('/usuarios/:id', (req, resp) => {
+  console.log(req.params.id)
+  console.log(req.body)
+  resp.send('<h1>Parabéns. Usuário Alterado!!!</h1>')
 })
 
 app.listen(3003)
