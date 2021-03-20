13c969e72c4c2e7896ed80755f669163a2af0292
Felipe Gomes Vicente <felipegomes.vicente2@gmail.com>
2021-03-19 21:51:04 -0300

    express parte 2

--- node/projeto/src/servidor.js
+++ node/projeto/src/servidor.js
@@ -2,6 +2,7 @@ const porta = 3003
 
 const express = require('express')
 const app = express()
+const bancoDeDados = require('./bancoDeDados')
 
 /*app.get('/produtos', (req, res, next) => {
   console.log('Middleware 1...')
@@ -9,7 +10,19 @@ const app = express()
 })*/
 
 app.get('/produtos', (req, res, next) => {
-  res.send({ nome: 'Notebook', preco: 123.45 }) // Converter p/ JSON
+  res.send(bancoDeDados.getProdutos())
+})
+
+app.get('/produtos/:id', (req, res, next) => {
+  res.send(bancoDeDados.getProduto(req.params.id))
+})
+
+app.post('/produtos', (req, res, next) => {
+  const produto = bancoDeDados.salvarProduto({
+    nome: req.body.name,
+    preco: req.body.preco
+  })
+  res.send(produto) // JSON
 })
 
 app.listen(porta, () => {
