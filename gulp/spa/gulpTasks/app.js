13b1665476ccc3c169412988f394eca2318e901b
Felipe Gomes Vicente <felipegomes.vicente2@gmail.com>
2021-04-10 09:14:30 -0300

    gulp

--- gulp/spa/gulpTasks/app.js
+++ gulp/spa/gulpTasks/app.js
@@ -7,12 +7,18 @@ const concat = require('gulp-concat')
 const htmlmin = require('gulp-htmlmin')
 
 
-function appHTML(cb) {
-  return cb()
+function appHTML() {
+  return gulp.src('src/**/*.html')
+    .pipe(htmlmin({ collapseWhitespace: true}))
+    .pipe(gulp.dest('build'))
 }
 
-function appCSS(cb) {
-  return cb()
+function appCSS() {
+  return gulp.src('src/assets/sass/index.scss')
+      .pipe(sass().on('error', sass.logError))
+      .pipe(uglifycss({ "uglyComments": true }))
+      .pipe(concat('app.min.css'))
+      .pipe(gulp.dest('build/assets/css'))
 }
 
 function appJS(cb) {
