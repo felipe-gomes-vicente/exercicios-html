4229b2aa4e10bc5cbff7486c567e99d2b4ef3c7d
Felipe Gomes Vicente <felipegomes.vicente2@gmail.com>
2021-04-10 09:03:32 -0300

    gulp

--- gulp/spa/gulpTasks/app.js
+++ gulp/spa/gulpTasks/app.js
@@ -1,4 +1,10 @@
 const gulp = require('gulp')
+const babel = require('gulp-babel')
+const uglify = require('gulp-uglify')
+const sass = require('gulp-sass')
+const uglifycss = require('gulp-uglifycss')
+const concat = require('gulp-concat')
+const htmlmin = require('gulp-htmlmin')
 
 
 function appHTML(cb) {
