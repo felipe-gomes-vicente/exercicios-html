0fea567340174b04b20401583271720fa3218db3
Felipe Gomes Vicente <felipegomes.vicente2@gmail.com>
2021-04-13 00:40:10 -0300

    webpack

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -4,7 +4,16 @@ module.exports = {
   mode: 'development',
   entry: './src/principal.js',
   output: {
-    filename: 'principal.js',
-    path: __dirname + '/public'
+      filename: 'principal.js',
+      path: __dirname + '/public'
+  },
+  module: {
+    rules: [{
+      test: /\.css$/, 
+      use: [
+        'style-loader', // Adiciona CSS a DOM injetando a tag <style>
+        'css-loader' // Interpreta @import, url()...
+      ]     
+    }]
   }
 }
