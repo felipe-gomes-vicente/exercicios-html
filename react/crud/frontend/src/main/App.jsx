f3eac9c60fe5968dc514eb7fe247a9f86640d639
Felipe Gomes Vicente <felipegomes.vicente2@gmail.com>
2021-04-29 22:33:23 -0300

    crud

--- react/crud/frontend/src/main/App.jsx
+++ react/crud/frontend/src/main/App.jsx
@@ -2,7 +2,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'font-awesome/css/font-awesome.min.css'
 import './App.css'
 import React from 'react'
-import { BrowserRouter } from 'react-router-dom' 
+import { BrowserRouter } from 'react-router-dom'
 
 import Logo from '../components/template/Logo'
 import Nav from '../components/template/Nav'
@@ -10,11 +10,11 @@ import Routes from './Routes'
 import Footer from '../components/template/Footer'
 
 export default props =>
-  <BrowserRouter>
-    <div className="app">
-      <Logo />
-      <Nav />
-      <Routes />
-      <Footer />
-    </div>
-  </BrowserRouter>
+    <BrowserRouter>
+        <div className="app">
+            <Logo />
+            <Nav />
+            <Routes />
+            <Footer />
+        </div>
+    </BrowserRouter>
