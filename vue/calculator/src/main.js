ec0943752e4297cd6ef2fd3c506b1d6c0be896f5
Felipe Gomes Vicente <felipegomes.vicente2@gmail.com>
2021-06-23 13:10:40 -0300

    Calculator

---
+++ vue/calculator/src/main.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import App from './App'
+
+new Vue({
+  render: h => h(App)
+}).$mount("#app")
+
+// new Vue({
+//   el: '#app',
+//   render(createElement) {
+//     return createElement(App)
+//   }
+// })
+
+/** Outra forma */
+// new Vue({
+//   render(createElement) {
+//     return createElement(App)
+//   }
+// }).$mount("#app")
